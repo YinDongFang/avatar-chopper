4842d10d89d18c4d01b53d2b18094d1eef660819
YinDongfang <1136005348@qq.com>
2025-02-08 22:18:30 +0800

    Basic structure

---
+++ packages/avatar-editor-react/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AvatarEditor from 'avatar-editor';
+
+function App() {
+  return <div>React Avatar Editor Demo</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root')); 
---
+++ packages/avatar-editor-vue/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import AvatarEditor from 'avatar-editor';
+
+const App = {
+  template: `<div>Vue Avatar Editor Demo</div>`
+};
+
+createApp(App).mount('#app'); 
