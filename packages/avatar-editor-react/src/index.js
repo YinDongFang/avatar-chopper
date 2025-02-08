2d26b2836d37a7716a497c24f890e1fa4f67053f
YinDongfang <1136005348@qq.com>
2025-02-08 22:19:25 +0800

    support typescript for react

--- packages/avatar-editor-react/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AvatarEditor from 'avatar-editor';
-
-function App() {
-  return <div>React Avatar Editor Demo</div>;
-}
-
-ReactDOM.render(<App />, document.getElementById('root')); 
