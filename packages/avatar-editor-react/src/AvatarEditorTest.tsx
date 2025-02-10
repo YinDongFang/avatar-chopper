b1968d443eea5f1cab6ddbda9ad587cc9bb95eef
--list <--global>
2025-02-10 18:44:47 +0800

    change test component

--- packages/avatar-editor-react/src/AvatarEditorTest.tsx
+++ packages/avatar-editor-react/src/AvatarEditorTest.tsx
@@ -3,13 +3,15 @@ import AvatarEditor from "../../avatar-editor/src/index";
 
 const AvatarEditorTest: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const editorRef = useRef<AvatarEditor | null>(null);
 
   useEffect(() => {
-    if (canvasRef.current) {
-      const editor = new AvatarEditor(canvasRef.current, {
-        image: "https://example.com/path/to/image.jpg", // 替换为实际图片URL
+    if (canvasRef.current && !editorRef.current) {
+      editorRef.current = new AvatarEditor(canvasRef.current, {
+        image: "https://picsum.photos/1920/1080", // 使用1920x1080尺寸的图片
         size: 300,
         maxScale: 2,
+        scale: 2,
         onLoadSuccess: (image) => {
           console.log("Image loaded successfully:", image);
         },
@@ -23,12 +25,12 @@ const AvatarEditorTest: React.FC = () => {
           console.log("Scale changed:", scale);
         },
       });
-
-      // 清理函数
-      return () => {
-        // 如果需要，可以在这里执行清理操作
-      };
     }
+
+    // 清理函数
+    return () => {
+      editorRef.current = null; // 清理 editor 实例
+    };
   }, []);
 
   return <canvas ref={canvasRef} width={400} height={400} />;
