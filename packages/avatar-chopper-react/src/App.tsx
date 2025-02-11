182dcf2d0bf9e8e41287fd7d2b8a23205f85efbb
Your Name <you@example.com>
2025-02-11 22:24:44 +0800

    move test code to App.tsx

--- packages/avatar-chopper-react/src/App.tsx
+++ packages/avatar-chopper-react/src/App.tsx
@@ -1,7 +1,18 @@
 import AvatarChopper from "./AvatarChopper";
 
 function App() {
-  return <AvatarChopper />;
+  return <AvatarChopper
+    width={1000}
+    height={1000}
+    props={{
+      image: "https://picsum.photos/1920/1080", // 使用1920x1080尺寸的图片
+      size: 300,
+      maxScale: 2,
+    }}
+    style={{
+      width: "100%", height: "100%",
+    }}
+  />
 }
 
 export default App;
--- packages/avatar-chopper-react/src/AvatarChopper.tsx
+++ packages/avatar-chopper-react/src/AvatarChopper.tsx
@@ -1,56 +1,27 @@
-import React, { useRef, useEffect, useState } from "react";
-import Chopper from "../../avatar-chopper/src/index";
+import React, { useRef, useEffect } from "react";
+import Chopper, { AvatarChopperOptions } from "../../avatar-chopper/src/index";
 
-const AvatarChopper: React.FC = () => {
+type AvatarChopperProps = {
+  props?: AvatarChopperOptions;
+} & React.CanvasHTMLAttributes<HTMLCanvasElement>;
+
+const AvatarChopper: React.FC<AvatarChopperProps> = ({ props, ...restProps }: AvatarChopperProps) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const editorRef = useRef<Chopper | null>(null);
 
-  const [offset, setOffset] = useState<any>();
-  const [scale, setScale] = useState<number>();
-
-  useEffect(() => {
-    editorRef.current?.setOptions({ offset, scale });
-  }, [offset, scale]);
-
   useEffect(() => {
     if (canvasRef.current && !editorRef.current) {
-      editorRef.current = new Chopper(canvasRef.current, {
-        image: "https://picsum.photos/1920/1080", // 使用1920x1080尺寸的图片
-        size: 300,
-        maxScale: 2,
-        scale: 1.5,
-        offset: { x: 100, y: 100 },
-        onLoadSuccess: (image) => {
-          console.log("Image loaded successfully:", image);
-        },
-        onLoadFailure: () => {
-          console.error("Failed to load image");
-        },
-        onOffsetChange: (offset) => {
-          console.log("Offset changed:", offset);
-          setOffset(offset);
-        },
-        onScaleChange: (scale) => {
-          console.log("Scale changed:", scale);
-          setScale(scale);
-        },
-      });
-    }
+      editorRef.current = new Chopper(canvasRef.current, { ...props });
 
-    // 清理函数
-    return () => {
-      editorRef.current?.destroy(); // 清理 editor 实例
-      editorRef.current = null;
-    };
+      return () => {
+        editorRef.current?.destroy();
+        editorRef.current = null;
+      };
+    }
   }, []);
 
   return (
-    <canvas
-      ref={canvasRef}
-      width={1000}
-      height={1000}
-      style={{ width: "100%", height: "100%" }}
-    />
+    <canvas ref={canvasRef} {...restProps} />
   );
 };
 
