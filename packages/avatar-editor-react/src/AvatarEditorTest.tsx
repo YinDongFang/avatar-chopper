b098ba5a31d03011e02e6552d83fac88c38c7492
--list <--global>
2025-02-11 12:27:47 +0800

    complete test component

--- packages/avatar-editor-react/src/AvatarEditorTest.tsx
+++ packages/avatar-editor-react/src/AvatarEditorTest.tsx
@@ -1,9 +1,16 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import AvatarEditor from "../../avatar-editor/src/index";
 
 const AvatarEditorTest: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const editorRef = useRef<AvatarEditor | null>(null);
+
+  const [offset, setOffset] = useState<any>();
+  const [scale, setScale] = useState<number>();
+
+  useEffect(() => {
+    editorRef.current?.setOptions({ offset, scale });
+  }, [offset, scale]);
 
   useEffect(() => {
     if (canvasRef.current && !editorRef.current) {
@@ -11,7 +18,8 @@ const AvatarEditorTest: React.FC = () => {
         image: "https://picsum.photos/1920/1080", // 使用1920x1080尺寸的图片
         size: 300,
         maxScale: 2,
-        scale: 2,
+        scale: 1.5,
+        offset: { x: 100, y: 100 },
         onLoadSuccess: (image) => {
           console.log("Image loaded successfully:", image);
         },
@@ -20,20 +28,30 @@ const AvatarEditorTest: React.FC = () => {
         },
         onOffsetChange: (offset) => {
           console.log("Offset changed:", offset);
+          setOffset(offset);
         },
         onScaleChange: (scale) => {
           console.log("Scale changed:", scale);
+          setScale(scale);
         },
       });
     }
 
     // 清理函数
     return () => {
-      editorRef.current = null; // 清理 editor 实例
+      editorRef.current?.destroy(); // 清理 editor 实例
+      editorRef.current = null;
     };
   }, []);
 
-  return <canvas ref={canvasRef} width={400} height={400} />;
+  return (
+    <canvas
+      ref={canvasRef}
+      width={1000}
+      height={1000}
+      style={{ width: "100%", height: "100%" }}
+    />
+  );
 };
 
 export default AvatarEditorTest;
