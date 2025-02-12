dd909145ebec8f530f38295da0c71990ace577a6
yindongfang <1136005348@qq.com>
2025-02-12 15:29:22 +0800

    change rollup and package config

--- packages/avatar-cropper-core/rollup.config.js
+++ packages/avatar-cropper-core/rollup.config.js
@@ -1,17 +1,13 @@
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: [
     {
-      file: 'dist/avatar-cropper.esm.js',
-      format: 'esm',
-      sourcemap: true
-    }
+      file: "dist/index.esm.js",
+      format: "esm",
+    },
   ],
-  plugins: [
-    resolve(),
-    typescript()
-  ]
-}; 
+  plugins: [resolve(), typescript()],
+};
