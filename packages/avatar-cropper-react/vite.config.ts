01258d6c4395be40d021343f39e6aca97d7ad9ad
yindongfang <1136005348@qq.com>
2025-02-12 15:45:35 +0800

    change react build config

--- packages/avatar-cropper-react/vite.config.ts
+++ packages/avatar-cropper-react/vite.config.ts
@@ -1,18 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import fs from "fs";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      root: __dirname,
+      include: ["src/AvatarCropper.tsx"],
+      afterBuild: () => {
+        const currentDirName = path.basename(__dirname);
+        const srcPath = path.resolve(
+          __dirname,
+          `dist/${currentDirName}/src/AvatarCropper.d.ts`
+        );
+        const destPath = path.resolve(__dirname, "dist/index.d.ts");
+        // 检查文件是否存在，然后移动文件
+        if (fs.existsSync(srcPath)) fs.renameSync(srcPath, destPath);
+        fs.rm(
+          path.resolve(__dirname, `dist/${currentDirName}`),
+          {
+            recursive: true,
+            force: true,
+          },
+          () => {}
+        );
+      },
+    }),
+  ],
   server: {
     port: 3001,
   },
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/AvatarCropper.tsx"),
-      name: "AvatarCropper",
-      fileName: "avatar-cropper",
+      fileName: "index",
       formats: ["es"], // 仅支持 ESM 格式
     },
     rollupOptions: {
